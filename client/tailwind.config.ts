d22f38322b73be6a34d388272dbd1efb248f410e
skerdicacaj <skerdicacaj@gmail.com>
2026-04-06 10:43:05 +0200

    implemented add template feature

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -41,6 +41,10 @@ const config: Config = {
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))"
         }
       },
       borderRadius: {
